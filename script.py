585118b0ee5addf0a0ffa13fa6dc4e607e385afd
Ivo Hashamov <63168603+ivohashamov@users.noreply.github.com>
2022-12-22 13:24:14 +0200

    add comments

--- script.py
+++ script.py
@@ -18,8 +18,16 @@ while True:
 
     cv2.imwrite('test.png' , frame) # save captured image
 
+    # run person detection script on captured image
+    # source - the image to analyse
+    # save_txt - whether coordiantes of detected objects should be saved in a txt file
+    # classes - which classes should be detected in the image - 0 belongs to the 'person' class
+    # conf_thresh - threshold for when an object is labeled as its class - 0.5 means 50% confidence
+    # exist_ok - whether the files from previous runs should be overwritten
     detect.run(source='test.png', save_txt=True, classes=0, conf_thres=0.5, exist_ok=True)
 
+    # count how many lines are in the detection file
+    # this is equivalent to the number of people detected
     num_persons = sum(1 for _ in open('./yolov5/runs/detect/exp/labels/test.txt'))
     current_timestamp = time.time()
     snapshot = {
@@ -29,8 +37,10 @@ while True:
     }
     print(snapshot)
 
+    # send snapshot to the server
     req = requests.post(SERVER_URL, json=snapshot)
 
+    # cleanup old files
     os.remove('./yolov5/runs/detect/exp/labels/test.txt')
     os.remove('test.png')
 
